68732ffb7fc0825f91b1824b911b95204b63bd7b
Kiba <kibade@gmail.com>
2024-09-02 18:22:30 +0100

    Just before fixing Max31855 python library error

--- display_temp/views.py
+++ display_temp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import random
+from max31855 import MAX31855, MAX31855Error
 # Create your views here.
 def say_hello(request):
-    return HttpResponse('Hello World')
+    my_variable = random.randint(1, 100)
+    return render(request, 'main.html', {'my_variable': my_variable})
 
