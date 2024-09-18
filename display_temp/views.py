cd7482069f0f02c6b449741de261ebe799f47307
tech <kdempsey@sd57.bc.ca>
2024-09-18 16:18:00 -0700

    Trying to move reading temperture out of views into main body

--- display_temp/views.py
+++ display_temp/views.py
@@ -4,19 +4,22 @@ from django.http import HttpResponse
 import random
 from max31855 import MAX31855, MAX31855Error
 from read_input import get_arm_position
+from read_temp import read_temp
+
 # Create your views here.
 def get_temp (request): 
 #####################################
 #Gets values to display on main page
 #####################################
-    cs_pin = 24 #MAX is using BOARD = pin numbers not BCM
-    clock_pin = 23
-    data_pin = 22
-    units = "c"
-    thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units)
-    temp = thermocouple.get() 
-    thermocouple.cleanup()
+#    cs_pin = 24 #MAX is using BOARD = pin numbers not BCM
+#    clock_pin = 23
+#    data_pin = 22
+#    units = "c"
+#    thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units)
+#    temp = thermocouple.get() 
+#    thermocouple.cleanup()
 #Get cat arm posistion info
+    temp = read_temp()
     cat_arm_value = get_arm_position()
 #    cat_arm_value = 0
     return render(request, 'main.html', {'temp': temp,'cat_arm_value': cat_arm_value })
---
+++ read_temp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from max31855 import MAX31855, MAX31855Error
+
+def read_temp (): 
+#####################################
+#Gets values to display on main page
+#####################################
+    cs_pin = 24 #MAX is using BOARD = pin numbers not BCM
+    clock_pin = 23
+    data_pin = 22
+    units = "c"
+    thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units)
+    temp = thermocouple.get() 
+    thermocouple.cleanup()
+    return(temp)
