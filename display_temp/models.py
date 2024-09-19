bb1a74a3a866dd18b1734e2516b81c46f39173e0
tech <kdempsey@sd57.bc.ca>
2024-09-18 20:37:11 -0700

    Minor coments

--- display_temp/models.py
+++ display_temp/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Temperture_Data(models.Model):
+    #Database for storing date and temperture
     date = models.DateTimeField(auto_now_add=True)
     temperture =models.IntegerField()
