a471919b32b5b64c810ebf1b8a08015e7cbb7f65
Kiba <kibade@gmail.com>
2024-09-23 16:29:13 -0700

    removed logo, added rules for when sound alert should happen in template

--- display_temp/views.py
+++ display_temp/views.py
@@ -10,11 +10,12 @@ from .models import Temperture_Data
 # Create your views here.
 def get_temp (request): 
     temp = read_temp() #reads thremocouple temp from Max31855
+
     # Save new recording to the database Temperture_Data
-    recorded_temp = Temperture_Data()
-    recorded_temp.temperture = temp
+#    recorded_temp = Temperture_Data()
+#    recorded_temp.temperture = temp
     ################
     cat_arm_value = get_arm_position()  #reading arm position
-    recorded_temp.arm = cat_arm_value
-    recorded_temp.save()
+#    recorded_temp.arm = cat_arm_value
+#    recorded_temp.save()
     return render(request, 'main.html', {'temp': temp,'cat_arm_value': cat_arm_value })
